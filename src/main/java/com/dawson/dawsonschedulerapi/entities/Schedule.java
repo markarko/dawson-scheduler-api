fb1713ee5bfb5bd1636818cf3b6684fff3deb1a4
Marko <markolitovchenko24@gmail.com>
2023-04-26 22:17:22 -0400

    Added schedule entity

--- src/main/java/com/dawson/dawsonschedulerapi/entities/Schedule.java
+++ src/main/java/com/dawson/dawsonschedulerapi/entities/Schedule.java
@@ -1,5 +1,20 @@
 package com.dawson.dawsonschedulerapi.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Schedule {
-    
+    private int dayOfWeek;
+    private Time startTime;
+    private Time endTime;
+    private String location;
 }
+
