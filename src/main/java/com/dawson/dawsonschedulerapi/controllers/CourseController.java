054361b1c103bca776a64dc1e065063993e8b6ed
Marko <markolitovchenko24@gmail.com>
2023-04-27 20:51:24 -0400

    Added route to choose course

--- src/main/java/com/dawson/dawsonschedulerapi/controllers/CourseController.java
+++ src/main/java/com/dawson/dawsonschedulerapi/controllers/CourseController.java
@@ -7,26 +7,43 @@ import com.dawson.dawsonschedulerapi.entities.Course;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
-@RestController()
+@RestController
 @RequestMapping("/courses")
 public class CourseController {
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getCourse(@RequestParam String courseNumber) {
-        Optional<Course> course = Filters.getCourseByCourseNumber(courseNumber);
-        if (course.isEmpty()) {
+    public ResponseEntity<Object> getCourse(@RequestParam("course-number") String courseNumber) {
+        List<Course> courses = Filters.getCourseByPartialCourseNumber(courseNumber);
+        if (courses.size() == 0){
             String error = "no courses found";
             HttpStatus status = HttpStatus.NOT_FOUND;
             return ResponseHandler.generateResponse(status, null, error);
         }
         HttpStatus status = HttpStatus.OK;
-        return ResponseHandler.generateResponse(status, course.get(), null);
+        return ResponseHandler.generateResponse(status, courses, null);
+    }
+
+    @RequestMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> addChoseCourse(@RequestParam("course-number") String courseNumber) {
+        Optional<Course> course = Filters.getCourseByCourseNumber(courseNumber);
+        if (course.isEmpty()){
+            String error = "this course doesn't exist";
+            HttpStatus status = HttpStatus.NOT_FOUND;
+            return ResponseHandler.generateResponse(status, null, error);
+        }
+        if (CourseManager.courseAlreadyChosen(course.get())){
+            String error = "this course is already chosen";
+            HttpStatus status = HttpStatus.CONFLICT;
+            return ResponseHandler.generateResponse(status, null, error);
+        }
+        CourseManager.addCourse(course.get());
+        HttpStatus status = HttpStatus.CREATED;
+        return ResponseHandler.generateResponse(status, course, null);
     }
 }
