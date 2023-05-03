060cc447b516aa00276265b388b9602fdef951c9
Marko <markolitovchenko24@gmail.com>
2023-05-03 11:02:35 -0400

    Allowed post method

--- src/main/java/com/dawson/dawsonschedulerapi/CORSConfig.java
+++ src/main/java/com/dawson/dawsonschedulerapi/CORSConfig.java
@@ -14,7 +14,7 @@ public class CORSConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500")
-                        .allowedMethods("GET")
+                        .allowedMethods("GET", "POST")
                         .allowedHeaders("*");
             }
         };
